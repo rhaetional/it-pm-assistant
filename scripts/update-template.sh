f5e71abe3c3b14e239b21b02547c4bf8dd5a02ee
Rhaetional <12344096+rhaetional@users.noreply.github.com>
2026-04-02 18:31:59 +0200

    chore: update framework

--- scripts/update-template.sh
+++ scripts/update-template.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 git fetch template
-git subtree pull --prefix=framework/ template framework-only --squash
+git subtree pull --prefix=framework/ template framework-only --squash -m "chore: update framework"
